efd8e7f2128d2845db9d95458975bd023a9419e6
rbonghi <raffaello.bonghi@officinerobotiche.it>
2015-07-07 14:50:42 +0200

    added draw function

--- js/console/rosmap.js
+++ js/console/rosmap.js
@@ -9,30 +9,38 @@ var ROSMAP = ROSMAP || {
 ROSMAP.Editor = function(options) {
 	var that = this;
 	options = options || {};
-	var ros = options.ros;
+	//var ros = options.ros;
 	this.rootObject = options.rootObject || new createjs.Container();
-	
-	this.color = '#000000';
-	this.stroke = 10;
-	this.drawingCanvas = new createjs.Shape();
 
-	this.rootObject.addChild(this.drawingCanvas);
-	
-	this.handleMouseMove = function(event) {
-		if (!event.primary) { return; }
-		console.log("Move");
-		var midPt = new createjs.Point(that.oldPt.x + event.stageX >> 1, that.oldPt.y + event.stageY >> 1);
-		that.drawingCanvas.graphics.clear().setStrokeStyle(that.stroke, 'round', 'round').beginStroke(that.color).moveTo(midPt.x, midPt.y).curveTo(that.oldPt.x, that.oldPt.y, that.oldMidPt.x, that.oldMidPt.y);
-		
-		that.oldPt.x = event.stageX;
+	// draw the arrow
+	this.graphics = new createjs.Graphics();	
+    this.oldPt;
+    this.oldMidPt;
+  
+    this.strokeSize = options.strokeSize || 3;
+    this.strokeColor = options.strokeColor || createjs.Graphics.getRGB(0, 0, 0);
+    
+    this.handleMouseMove = function(event) {
+        if (!event.primary) { return; }
+        //var pos_tmp = that.rootObject.globalToRos(event.stageX, event.stageY);
+		//var pose = new ROSLIB.Vector3(pos_tmp);
+        var midPt = new createjs.Point(that.oldPt.x + event.stageX >> 1, that.oldPt.y + event.stageY >> 1);
+        
+        that.graphics.setStrokeStyle(that.strokeSize);
+        that.graphics.beginStroke(that.strokeColor);
+        that.graphics.moveTo(midPt.x, midPt.y);
+        
+        console.log("OLD [" + that.oldPt.x + "," + that.oldPt.y + "] - MID [" + that.oldMidPt.x + "," + that.oldMidPt.y + "]");
+        that.graphics.curveTo(that.oldPt.x, that.oldPt.y, that.oldMidPt.x, that.oldMidPt.y);
+        // create the shape
+        createjs.Shape.call(that, that.graphics);
+        /// Update all points
+        that.oldPt.x = event.stageX;
 		that.oldPt.y = event.stageY;
-	
 		that.oldMidPt.x = midPt.x;
 		that.oldMidPt.y = midPt.y;
-		
-		this.rootObject.update();
-	};
-	
+    };
+    
 	var handleMouseUp = function(event) {
 		if (!event.primary) { return; }
 		that.rootObject.removeEventListener("stagemousemove", that.handleMouseMove);
@@ -40,16 +48,23 @@ ROSMAP.Editor = function(options) {
 	
 	var handleMouseDown = function(event) {
 		if (!event.primary) { return; }
+		//var pos_tmp = that.rootObject.globalToRos(event.stageX, event.stageY);
+		//var pose = new ROSLIB.Vector3(pos_tmp);
 		that.oldPt = new createjs.Point(event.stageX, event.stageY);
 		that.oldMidPt = that.oldPt.clone();
-		console.log("Down");
 		that.rootObject.addEventListener("stagemousemove", that.handleMouseMove);
 	};
 	
 	this.rootObject.addEventListener("stagemousedown", handleMouseDown);
 	this.rootObject.addEventListener("stagemouseup", handleMouseUp);
+
+	this.color = '#000000';
+	this.stroke = 10;
 	
 	console.log("Load ROSMAP");
 	
+	this.y = -this.rootObject.canvas.width;
+	this.rootObject.addChild(this);
+	
 };
-//ROSMAP.Editor.prototype.__proto__ = createjs.Bitmap.prototype;
+ROSMAP.Editor.prototype.__proto__ = createjs.Shape.prototype;

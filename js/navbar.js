e307da8b7e0bbb25e3a24de8798d4bb418e61d81
Raffaello Bonghi <raffaello@rnext.it>
2019-06-29 17:36:19 +0200

    Fix license

--- js/navbar.js
+++ js/navbar.js
@@ -1,57 +1,24 @@
+/* 
+ * This file is part of the ros_webconsole package (https://github.com/rbonghi/ros_webconsole or http://rnext.it).
+ * Copyright (c) 2019 Raffaello Bonghi.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var ros_pages = ros_pages || {
     REVISION: '0.0.1'
 };
-
-ros_pages.navbar = function(pages) {
-    var bar = "";
-    // Make menu
-    if (pages.length > 0) {
-        bar += '<ul>';
-        for(var i = 0; i < pages.length; i++) {
-            // Extract properties
-            var id = pages[i].id;
-            var name = pages[i].name;
-            var icon = pages[i].icon;
-            // Add element
-            var element = '<li><a href="#' + id + '" data-icon="' + icon + '" data-transition="fade">' + name + '</a></li>';
-            // add in string
-            bar += element;
-        }
-        bar += '</ul>';
-    }
-    return bar;
-};
-
-ros_pages.header = function(name, pages) {
-    // Build navbar
-    var bar = ros_pages.navbar(pages);
-    // Build header
-    var header = '<div data-role="header" data-theme="a" data-position="fixed">' +
-                 '<h1>' + name + '</h1>' + 
-                 '<div data-role="navbar" id="menu">' + bar + '</div>' +
-                 '</div>';
-    // Append header
-    $(header).prependTo('body').enhanceWithin();
-};
-
-ros_pages.footer = function() {
-    var footer = '<div data-role="footer" data-theme="a">' +
-                    '<p>RB</p>' +
-                 '</div>';
-    $(footer).prependTo('body').enhanceWithin();
-};
-
-ros_pages.make = function(pages) {
-    newPages = '';
-    for(var i = 0; i < pages.length; i++) {
-        var name = pages[i].name;
-        newPages += '<div data-role="page" id="' + name + '" data-title="' + name +'" data-icon="navigation">' + 
-                        '<h1>' + name + '</h1>' +
-                    '</div>';
-    }
-    // Append header
-    $(newPages).prependTo('body').enhanceWithin();
-}
 
 ros_pages.find = function() {
 	// Find pages
@@ -65,6 +32,14 @@ ros_pages.find = function() {
 	    pages.push(page);
 	}
     return pages;
+}
+
+ros_pages.connect = function(e) {
+    console.log("Connect: " + e);
+}
+
+ros_pages.disconnect = function(e) {
+    console.log("Disconnect: " + e);
 }
 
 ros_pages.controller = function(name, pages) {
@@ -125,4 +100,55 @@ ros_pages.isMobile = function() {
 		}
 	};
 	return isMobile;
-};
+}
+
+ros_pages.navbar = function(pages) {
+    var bar = "";
+    // Make menu
+    if (pages.length > 0) {
+        bar += '<ul>';
+        for(var i = 0; i < pages.length; i++) {
+            // Extract properties
+            var id = pages[i].id;
+            var name = pages[i].name;
+            var icon = pages[i].icon;
+            // Add element
+            var element = '<li><a href="#' + id + '" data-icon="' + icon + '" data-transition="fade">' + name + '</a></li>';
+            // add in string
+            bar += element;
+        }
+        bar += '</ul>';
+    }
+    return bar;
+}
+
+ros_pages.header = function(name, pages) {
+    // Build navbar
+    var bar = ros_pages.navbar(pages);
+    // Build header
+    var header = '<div data-role="header" data-theme="a" data-position="fixed">' +
+                 '<h1>' + name + '</h1>' + 
+                 '<div data-role="navbar" id="menu">' + bar + '</div>' +
+                 '</div>';
+    // Append header
+    $(header).prependTo('body').enhanceWithin();
+}
+
+ros_pages.footer = function() {
+    var footer = '<div data-role="footer" data-theme="a">' +
+                    '<p>RB</p>' +
+                 '</div>';
+    $(footer).prependTo('body').enhanceWithin();
+}
+
+ros_pages.make = function(pages) {
+    newPages = '';
+    for(var i = 0; i < pages.length; i++) {
+        var name = pages[i].name;
+        newPages += '<div data-role="page" id="' + name + '" data-title="' + name +'" data-icon="navigation">' + 
+                        '<h1>' + name + '</h1>' +
+                    '</div>';
+    }
+    // Append header
+    $(newPages).prependTo('body').enhanceWithin();
+}

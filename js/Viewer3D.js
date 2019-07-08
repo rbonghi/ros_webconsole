e8a44c82f56c5fcdf18c21b8e9fa9428bfdb32cc
Raffaello Bonghi <raffaello@rnext.it>
2019-07-08 18:13:51 +0200

    Improve view3D

--- js/Viewer3D.js
+++ js/Viewer3D.js
@@ -29,9 +29,41 @@ Viewer3D.Map3D = function(file, options) {
     this.ros = options.ros;
     // Page configuration
     this.divID = options.divID || 'map-3D';
+    this.view3D = options.view3D || 'view3D-list';
+    this.view3Dframe = options.view3Dframe || '#view3D-frame';
     // Load page size
     this.size = options.size;
-		
+    
+    $( this.view3Dframe ).bind( "change paste", function(event, ui) {
+        var value = $(this).val();
+        if(that.tfClient) {
+            console.log("New frame " + value);
+            // Update configuration
+            that.config.frame = value
+            // Change frame
+            that.tfClient.fixedFrame = value;
+            // Save the local storage for this configuration
+            window.localStorage.setItem('view3D', JSON.stringify(that.config));
+        }
+    });
+    
+    // Control element
+    $('#view3D-element').children('li').bind('touchstart mousedown', function(e) {
+        var name = $(this).text();
+        var href = $(this).children('a').attr('href').split('#')[1];
+        var nextId = that.config.objects.length;
+        // Define object
+        obj = {'name': nextId + ' ' + name,
+               'type': name,
+               'id': href + nextId};
+        // Update configuration
+        that.config.objects.push(obj);
+        // Save the local storage for this configuration
+        window.localStorage.setItem('view3D', JSON.stringify(that.config));
+        // Add in list
+        that.addList(obj, nextId);
+    });
+
     // Json load and config
     $.getJSON( file ).done(function( json ) {
         // Load ROS configuration file
@@ -56,6 +88,37 @@ Viewer3D.Map3D = function(file, options) {
 	});
 }
 
+Viewer3D.Map3D.prototype.addList = function(obj, idx) {
+    var that = this;
+    // Extract name
+    var name = obj.name;
+    var id = obj.id;
+    // Make Collapsible
+    var content = '<div data-role="collapsible" id="' + this.view3D + '-' + id + '">' + 
+                    '<h3>' + name + "</h3>" +
+                    '<p>I am the collapsible content in a set so this feels like an accordion.</p>' +
+                  '</div>';
+    // Add content collapsible
+    $( '#' + this.view3D ).append( content );
+    // Add remove button
+    $('#' + this.view3D + '-' + id).append(function() {
+        // Add remove function
+        return $('<a href="#remove" class="ui-btn ui-shadow ui-corner-all ui-icon-minus ui-btn-icon-left">Remove</a>').click(function() {
+            // remove selected element in list
+            that.config.objects.splice(idx, 1);
+            // Save the local storage for this configuration
+            window.localStorage.setItem('view3D', JSON.stringify(that.config));
+            // Remove collapsible
+            $('#' + that.view3D + '-' + id).remove();
+            // Refresh collapsible
+            $( '#' + that.view3D ).collapsibleset( "refresh" );
+        });
+    });
+    // Update collapsible
+    $( '#' + this.view3D ).collapsibleset( "refresh" );
+    
+}
+
 Viewer3D.Map3D.prototype.make = function() {
     var that = this;
     // Initialize sessionv view3D informations
@@ -65,12 +128,12 @@ Viewer3D.Map3D.prototype.make = function() {
         this.sview3D = JSON.parse(sessionStorage.getItem('view3D'));
     }
 	// Create a TF client that subscribes to the fixed frame.
-	var tfClient = new ROSLIB.TFClient({
+	this.tfClient = new ROSLIB.TFClient({
 		ros: this.ros.ros,
 		angularThres: 0.01,
 		transThres: 0.01,
-		rate: this.robot.rate,
-		fixedFrame: this.robot.frame
+		rate: this.config.rate,
+		fixedFrame: this.config.frame
 	});
     // Create the main viewer.
     this.viewer = new ROS3D.Viewer({
@@ -86,11 +149,17 @@ Viewer3D.Map3D.prototype.make = function() {
         that.sview3D.cameraPose = that.viewer.camera.position;
         window.sessionStorage.setItem('view3D', JSON.stringify(that.sview3D));
     });
+    // Populate object list
+    for(var i = 0; i < this.config.objects.length; i++) {
+        var obj = this.config.objects[i];
+        //Add in list
+        this.addList(obj, i);
+    }
     // Setup the map client.
     var gridClient = new ROS3D.OccupancyGridClient({
       ros : this.ros.ros,
       rootObject : this.viewer.scene,
-      tfClient: tfClient,
+      tfClient: this.tfClient,
       continuous: true
     });
     var grid = new ROS3D.Grid({
@@ -102,10 +171,11 @@ Viewer3D.Map3D.prototype.make = function() {
 	// Add the URDF model of the robot.
 	var urdfClient = new ROS3D.UrdfClient({
 		ros: this.ros.ros,
-		tfClient: tfClient,
+		tfClient: this.tfClient,
 		path: this.ros.config.protocol + '//' + this.ros.config.server + ':' + this.ros.config.meshport + '/',
 		rootObject: this.viewer.scene,
-		loader: ROS3D.STL_LOADER
+		loader: ROS3D.STL_LOADER,
+		param: 'minicar/robot_description'
 	});
 };
 
@@ -128,6 +198,11 @@ Viewer3D.Map3D.prototype.loadConfig = function(json) {
     }
     this.config = {};
     this.config.background = config.background || '#EEEEEE';
+    this.config.rate = config.rate || 10.0;
+    this.config.frame = config.frame || 'base_link';
+    this.config.objects = config.objects || [];
+    // Set text ros URL
+    $( this.view3Dframe ).val(this.config.frame);
     // Save the local storage for this configuration
     window.localStorage.setItem('view3D', JSON.stringify(this.config));
 }
